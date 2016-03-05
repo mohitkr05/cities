71d5c9b3949afea586ce47f2b0ea4dc2b0b4ee9e
mohitkr05 <mohitkr05@gmail.com>
2016-03-05 11:25:56 +0530

    first commit

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package cities
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'cities' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'cities' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'cities' ), 'cities', '<a href="http://kumarmohit.com" rel="designer">Mohit Kumar</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
